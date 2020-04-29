01df64117dea7828f9db4f38b64097769410dc46
Italo <italo_bruno91@hotmail.com>
2020-04-28 23:33:44 -0300

    :sparkles: feat: update class project

--- week02/04-gym-manager/class-project/instructors.js
+++ week02/04-gym-manager/class-project/instructors.js
@@ -1,7 +1,32 @@
 const fs = require('fs');
 
 const data = require('./data.json');
+const { age, date } = require('./utils');
 
+// show
+exports.show = function(req, res) {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find(function(instructor) {
+    return instructor.id == id;
+  });
+
+  if (!foundInstructor) {
+    return res.send('Instructor not found!');
+  }
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    gender: foundInstructor.gender,
+    services: foundInstructor.services.split(', '),
+    created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
+  }
+
+  return res.render('instructors/show', { instructor });
+}
+
+// create
 exports.post = function(req, res) {
   const keys = Object.keys(req.body);
 
@@ -34,3 +59,23 @@ exports.post = function(req, res) {
 
   return res.send('recebido');
 };
+
+// edit
+exports.edit = function(req, res) {
+  const { id } = req.params;
+
+  const foundInstructor = data.instructors.find(function(instructor) {
+    return instructor.id == id;
+  });
+
+  if (!foundInstructor) {
+    return res.send('Instructor not found!');
+  }
+
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth)
+  }
+  
+  return res.render('instructors/edit', { instructor });
+}
