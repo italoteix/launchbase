c3f9e37c55f3697347abd566c1bbce9df1781f7e
Italo <italo_bruno91@hotmail.com>
2020-04-30 22:19:04 -0300

    :sparkles: feat: update class project

--- week02/04-gym-manager/class-project/server.js
+++ week02/04-gym-manager/class-project/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const methodOverride = require('method-override');
 
 const routes = require('./routes');
 
@@ -7,14 +8,14 @@ const server = express();
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'));
+server.use(methodOverride('_method'))
+server.use(routes);
 
 server.set('view engine', 'njk');
 nunjucks.configure('views', {
   express: server,
   noCache: true
 });
-
-server.use(routes);
 
 server.listen(5000, function() {
   console.log('Server runnig on port 5000');
