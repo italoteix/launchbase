8c6f4a88887ac446c1b9ee2e0dfeae2192822f74
Italo <italo_bruno91@hotmail.com>
2020-05-21 23:01:54 -0300

    :sparkles: feat: update class project

--- week03/05-persisting-application-data/class-project/public/scripts.js
+++ week03/05-persisting-application-data/class-project/public/scripts.js
@@ -6,3 +6,60 @@ for (item of menuItens) {
     item.classList.add('active');
   }
 }
+
+// Pagination
+function paginate(selectedPage, totalPages) {
+  let pages = [],
+      oldPage;
+  
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  
+    const firstAndLastPage = currentPage === 1 || currentPage === totalPages;
+    const pageAfterSelectedPage = currentPage <= selectedPage + 2;
+    const pageBeforeSelectedPage = currentPage >= selectedPage - 2;
+  
+    if (firstAndLastPage || pageAfterSelectedPage && pageBeforeSelectedPage) {
+  
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push('...');
+      }
+      
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1);
+      }
+  
+      pages.push(currentPage);
+  
+      oldPage = currentPage;
+    }
+  }
+
+  return pages;
+}
+
+const pagination = document.querySelector('.pagination');
+
+function createPagination(pagination) {
+  const filter = +pagination.dataset.filter;
+  const page = +pagination.dataset.page;
+  const total = +pagination.dataset.total;
+  const pages = paginate(page, total);
+  
+  let elements = "";
+  
+  for (let page of pages) {
+    if (String(page).includes('...')) {
+      elements += `<span>${page}</span>`;
+    } else {
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}" >${page}</a>`;
+      } else {
+        elements += `<a href="?page=${page}" >${page}</a>`;
+      }
+    }
+  }
+  
+  pagination.innerHTML = elements;
+}
+
+if (pagination) createPagination(pagination);
--- week03/05-persisting-application-data/class-project/src/app/models/Instructor.js
+++ week03/05-persisting-application-data/class-project/src/app/models/Instructor.js
@@ -95,5 +95,39 @@ module.exports = {
 
       return callback();
     });
+  },
+  paginate(params) {
+    const { filter, limit, offset, callback } = params;
+
+    let query = "",
+        filterQuery = "",
+        totalQuery = `(
+          SELECT count(*) from instructors
+        ) AS total`;
+
+    if (filter) {
+      filterQuery = `
+      WHERE instructors.name ILIKE '%${filter}%'
+      OR instructors.services ILIKE '%${filter}%'
+      `;
+
+      totalQuery = `(
+        SELECT count(*) from instructors
+        ${filterQuery}
+      ) AS total`;
+    }
+
+    query = `
+    SELECT instructors.*, ${totalQuery}, count(members) AS total_students
+    FROM instructors
+    LEFT JOIN members on (instructors.id = members.instructor_id)
+    ${filterQuery}
+    GROUP BY instructors.id LIMIT $1 OFFSET $2`;
+
+    db.query(query, [limit, offset], function(err, results) {
+      if (err) throw `Database error! ${err}`;
+
+      callback(results.rows);
+    });
   }
 };
