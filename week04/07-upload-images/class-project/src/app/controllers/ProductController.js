bc1c61ea551990ba879a5b18c23914d301cba241
Italo <italo.brtx@gmail.com>
2020-07-02 00:00:03 -0300

    :sparkles: feat: update class project

--- week04/07-upload-images/class-project/src/app/controllers/ProductController.js
+++ week04/07-upload-images/class-project/src/app/controllers/ProductController.js
@@ -1,4 +1,4 @@
-const { formatPrice } = require('../../lib/utils')
+const { formatPrice, date } = require('../../lib/utils')
 
 const Category = require('../models/Category');
 const Product = require('../models/Product');
@@ -35,6 +35,31 @@ module.exports = {
     await Promise.all(filesPromise);
 
     return res.redirect(`/products/${productId}/edit`);
+  },
+  async show(req, res) {
+
+    let results = await Product.find(req.params.id);
+    const product = results.rows[0];
+
+    if (!product) return res.send('Product not found!');
+
+    const { day, hour, minutes, month} = date(product.updated_at);
+
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}h${minutes}`,
+    };
+    
+    product.oldPrice = formatPrice(product.old_price);
+    product.price = formatPrice(product.price);
+
+    results = await Product.files(product.id);
+    const files = results.rows.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+    }))
+
+    return res.render('products/show', { product, files });
   },
   async edit(req, res) {
     let results = await Product.find(req.params.id);
@@ -96,7 +121,7 @@ module.exports = {
 
     await Product.update(req.body);
 
-    return res.redirect(`/products/${req.body.id}/edit`);
+    return res.redirect(`/products/${req.body.id}`);
   },
   async delete(req, res) {
     await Product.delete(req.body.id);
